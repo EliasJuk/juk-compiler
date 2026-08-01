cdfa508c372951d5ee5a8dcb4a302ed7883e018e
eliasjuk <eliasp.juk@hotmail.com>
2026-08-01 17:37:17 -0300

    feat(lexer): recognize print token

--- compiler/src/lexer.c
+++ compiler/src/lexer.c
@@ -1,6 +1,9 @@
 #include "lexer.h"
 #include <stdio.h>
+#include <string.h>
 
 void lexer_analizer(const char *codigo){
-  printf("%s", codigo);
+  if(strncmp(codigo, "print", 5) == 0 ){
+    printf("TOKEN_PRINT\n");
+  }
 }
