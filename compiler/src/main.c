944a940992c6c10cca377bb0c27d6e2eb7626e54
eliasjuk <eliasp.juk@hotmail.com>
2026-08-01 16:02:54 -0300

    feat(compiler): allocate memory for source code

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
   if(argc < 2){
@@ -14,14 +15,31 @@ int main(int argc, char *argv[]){
   }
 
   printf("Arquivo aberto com sucesso!\n");
-  
+
   // Lê e exibe o arquivo caractere por caractere
   int caractere;
   while ((caractere = fgetc(arquivo)) != EOF) {
     printf("%c", caractere);
   }
 
+  // Tamanho do arquivo
+  fseek(arquivo, 0, SEEK_END);
+  long tamanho = ftell(arquivo);  
+  rewind(arquivo);
+  printf("Tamanho: %ld bytes\n", tamanho);
+
+  // Alocar memoria para o codigo
+  char *codigo = malloc(tamanho + 1);
+
+  if (codigo == NULL) {
+    printf("Nao foi possivel reservar memoria.\n");
+    fclose(arquivo);
+    return 1;
+  }  
+
+  // Liberar memoria
   fclose(arquivo);
+  free(codigo);
 
   return 0;
 }
