cdb2b3a9459891bd80a1003a009d758839abc6c4
eliasjuk <eliasp.juk@hotmail.com>
2026-08-01 17:51:41 -0300

    feat(lexer): read string literal

--- compiler/src/lexer.c
+++ compiler/src/lexer.c
@@ -6,4 +6,20 @@ void lexer_analizer(const char *codigo){
   if(strncmp(codigo, "print", 5) == 0 ){
     printf("TOKEN_PRINT\n");
   }
+  
+  if (codigo[5] == '(') {
+    printf("TOKEN_LEFT_PAREN\n");
+  }
+
+  if (codigo[6] == '"') {
+    printf("INICIO_STRING\n");
+  }
+
+  int i = 7;
+  while (codigo[i] != '"' && codigo[i] != '\0') {
+    printf("%c", codigo[i]);
+    i++;
+  }
+
+  printf("\n");
 }
