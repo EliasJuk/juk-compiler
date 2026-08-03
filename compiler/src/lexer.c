115cf6977b055b47b94dc2d63c72c0b59585d545
eliasjuk <eliasp.juk@hotmail.com>
2026-08-03 15:46:04 -0300

    feat(lexer): extract string literal

--- compiler/src/lexer.c
+++ compiler/src/lexer.c
@@ -1,6 +1,7 @@
 #include "lexer.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 static int skip_whitespace(const char *codigo, int i) {
   while (isspace((unsigned char) codigo[i])) {
@@ -9,7 +10,6 @@ static int skip_whitespace(const char *codigo, int i) {
 
   return i;
 }
-
 
 void lexer_analizer(const char *codigo){
   int i = 0;
@@ -35,10 +35,16 @@ void lexer_analizer(const char *codigo){
     i++;
   }
 
+  int inicio_string = i;
+  
+
   while (codigo[i] != '"' && codigo[i] != '\0') {
     printf("%c", codigo[i]);
     i++;
   }
+
+  int tamanho_string = i - inicio_string;
+  printf("\nTAMANHO_STRING: %d\n", tamanho_string);
 
   if (codigo[i] == '\0') {
     printf("\nERRO_LEXICO: string nao finalizada.\n");
@@ -62,4 +68,23 @@ void lexer_analizer(const char *codigo){
   if (codigo[i] == ';') {
     printf("TOKEN_SEMICOLON\n");
   }
+
+  // Reservar memória para armazenar a string
+  char *texto = malloc(tamanho_string + 1);
+
+  if (texto == NULL) {
+    printf("Erro ao reservar memoria para a string.\n");
+    return;
+  }
+
+  for (int j = 0; j < tamanho_string; j++) {
+    texto[j] = codigo[inicio_string + j];
+  }
+
+  texto[tamanho_string] = '\0';
+
+  printf("STRING: %s\n", texto);
+
+  // Libera a memoria
+  free(texto);
 }
